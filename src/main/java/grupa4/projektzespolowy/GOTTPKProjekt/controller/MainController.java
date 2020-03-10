3ef0b358f4673d66f4da3a6df315ab3784c272b0
Seldo97 <seldo97@gmail.com>
2020-03-10 03:55:15 +0100

    dodanie strony index

--- src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/controller/MainController.java
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/controller/MainController.java
@@ -2,13 +2,17 @@ package grupa4.projektzespolowy.GOTTPKProjekt.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
 
     @GetMapping("/")
-    public String indexPage() {
-        return "Hello World";
+    public ModelAndView indexPage() {
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+
     }
 
 }
