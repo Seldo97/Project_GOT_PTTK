42dcf7471fde3421dce4a4e4698e1d7c36174762
Paulina <p.nawrocka97@gmail.com>
2020-03-28 13:18:26 +0100

    Grupa
    Grupa - szkielet

---
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/controller/GrupaController.java
@@ -0,0 +1,31 @@
+package grupa4.projektzespolowy.GOTTPKProjekt.controller;
+
+import grupa4.projektzespolowy.GOTTPKProjekt.service.GrupaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class GrupaController {
+
+
+    @Autowired
+    private GrupaServiceImpl grupaServiceImpl;
+
+
+    @GetMapping("/grupy")
+    public ModelAndView getAllProduct(Authentication authentication) {
+
+        ModelAndView modelAndView = new ModelAndView("grupy/grupy");
+        modelAndView.addObject("grupy", grupaServiceImpl.getAllGrupa());
+        modelAndView.addObject("LoggedUser", authentication);
+
+        return modelAndView;
+    }
+
+
+}
---
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/model/Grupa.java
@@ -0,0 +1,48 @@
+package grupa4.projektzespolowy.GOTTPKProjekt.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "Grupa")
+public class Grupa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_grupa")
+    private int idGrupa;
+
+    @Column(name = "nazwa", nullable=false, length=50)
+    private String nazwa;
+
+
+    public Grupa() {
+    }
+
+    public Grupa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public int getIdGrupa() {
+        return idGrupa;
+    }
+
+    public void setIdGrupa(int idGrupa) {
+        this.idGrupa = idGrupa;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public void setNazwa(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    @Override
+    public String toString() {
+        return "Grupa{" +
+                "idGrupa=" + idGrupa +
+                ", nazwa='" + nazwa + '\'' +
+                '}';
+    }
+}
