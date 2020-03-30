87dda6a41b94f6e4fa8f7a7d326979b02c1069e0
Adam Zimny <adamczyk97@op.pl>
2020-03-30 02:20:50 +0200

    Poprawione bledy przy merge

--- src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/service/TurystaOdznakaService.java
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/service/TurystaOdznakaService.java
@@ -1,10 +1,7 @@
 package grupa4.projektzespolowy.GOTTPKProjekt.service;
 
-<<<<<<< HEAD
-=======
 import grupa4.projektzespolowy.GOTTPKProjekt.model.Odznaka;
 import grupa4.projektzespolowy.GOTTPKProjekt.model.Turysta;
->>>>>>> 67e40d8ba9eb8684e193b02f30a77419907a610d
 import grupa4.projektzespolowy.GOTTPKProjekt.model.TurystaOdznaka;
 
 import java.util.List;
--- src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/service/TurystaOdznakaServiceImpl.java
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/service/TurystaOdznakaServiceImpl.java
@@ -1,10 +1,8 @@
 package grupa4.projektzespolowy.GOTTPKProjekt.service;
 
-<<<<<<< HEAD
-=======
+
 import grupa4.projektzespolowy.GOTTPKProjekt.model.Odznaka;
 import grupa4.projektzespolowy.GOTTPKProjekt.model.Turysta;
->>>>>>> 67e40d8ba9eb8684e193b02f30a77419907a610d
 import grupa4.projektzespolowy.GOTTPKProjekt.model.TurystaOdznaka;
 import grupa4.projektzespolowy.GOTTPKProjekt.repository.TurystaOdznakaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
