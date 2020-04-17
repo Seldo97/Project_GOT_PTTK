42a21b6f282505c2f555ca7f2682e98b9fac8821
Seldo97 <seldo97@gmail.com>
2020-04-17 17:41:26 +0200

    wybieranie daty wycieczki od dzisiaj

--- src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/repository/ZdjecieWycieczkaRepository.java
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/repository/ZdjecieWycieczkaRepository.java
@@ -1,11 +1,17 @@
 package grupa4.projektzespolowy.GOTTPKProjekt.repository;
 
+import grupa4.projektzespolowy.GOTTPKProjekt.model.Wycieczka;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import grupa4.projektzespolowy.GOTTPKProjekt.model.ZdjecieWycieczka;
+import java.util.*;
 
 @Repository
 public interface ZdjecieWycieczkaRepository extends JpaRepository<ZdjecieWycieczka, Integer> {
+
+    List<ZdjecieWycieczka> getAllByWycieczka(Wycieczka wycieczka);
+
+    List<ZdjecieWycieczka> getAllByWycieczka_IdWycieczka(int idWycieczka);
 
 }
