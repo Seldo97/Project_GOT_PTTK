097bebebd9d034b8732784a7cd6742c2981638bf
kamilkrzywanski <kamilkrzywanski01@gmail.com>
2020-11-04 16:15:16 +0100

    Ulepszony formularz logowania

--- src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/SecurityConfiguration.java
+++ src/main/java/grupa4/projektzespolowy/GOTTPKProjekt/SecurityConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -69,7 +70,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and().
                 formLogin().loginPage("/login").defaultSuccessUrl("/").failureUrl("/login?error=true").
                 permitAll()
-
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/?success=true")
+                .and()
 
                // .anyRequest().hasRole("administrator")
 
